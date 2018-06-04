d9325573dc3d7f84fee4183579f4aa02302ad754
Vitor Cardoso <vitor.cardoso@pentaho.com>
2018-06-04 16:25:24 +0100

    ignore

--- core/src/main/java/org/pentaho/di/compatibility/Value.java
+++ core/src/main/java/org/pentaho/di/compatibility/Value.java
@@ -112,8 +112,6 @@ public class Value implements Cloneable, XMLInterface, Serializable {
   private String origin;
   private boolean NULL;
 
-  private boolean worked;
-
   /**
    * Constructs a new Value of type EMPTY
    *
